0f2723f253aa4bfd4c8b66ebc668ddd6daec52be
devthursday-tech <dev.thursday@gmail.com>
2026-01-31 01:04:53 +0530

    Bill_Split
    # Bill Split Calculator
    
    This Python program calculates how much each person needs to pay
    by dividing the total bill amount among given number of people.
    
    ## Features
    - User input based
    - Simple and beginner friendly
    - Real-life use case
    
    ## How to Run
    python bill_split.py

---
+++ Bill_Split.py
@@ -0,0 +1,4 @@
+Total_Bill_Amount = int(input('Enter Amount: '))
+Total_People = int(input('Enter Number of People:'))
+Per_People_bill = Total_Bill_Amount / Total_People
+print("Each People Should Pay" , Per_People_bill)
